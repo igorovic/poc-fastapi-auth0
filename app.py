503a274a10bb506f49e20c6794935180637abb5a
igorovic <758832+igorovic@users.noreply.github.com>
2021-06-03 14:56:58 +0000

    wip

--- app.py
+++ app.py
@@ -2,45 +2,93 @@ import os
 import json
 from typing import Optional
 import httpx
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, Security, status, HTTPException, Depends
+from fastapi.security import SecurityScopes
 from dotenv import load_dotenv
 from authlib.jose import JWTClaims, jwt, KeySet
+from authlib.jose.errors import DecodeError, ExpiredTokenError
+from oAuthTokenBearer import OAuth2TokenBearer
 
-
-from authlib.integrations.starlette_client import OAuth
 
 load_dotenv()
 app = FastAPI()
-oauth = OAuth(app)
+
 domain = os.getenv('AUTH0_DOMAIN')
 auth0_discovery_url = f'https://{domain}/.well-known/openid-configuration'
 
 
 ks = None
+auth0_config = None
 r = httpx.get(auth0_discovery_url)
 if r.status_code == 200:
     auth0_config = json.loads(r.text)
     jwks_resp = httpx.get(auth0_config['jwks_uri'])
     ks = KeySet(json.loads(jwks_resp.text)['keys'])
-
-    """ auth0 = oauth.register(
-        'auth0',
-        # client_id='YOUR_CLIENT_ID',
-        # client_secret='YOUR_CLIENT_SECRET',
-        api_base_url=f'https://{domain}',
-        access_token_url=f'https://{domain}/oauth/token',
-        authorize_url=f'https://{domain}/authorize',
-        # client_kwargs={
-        #    'scope': 'openid profile email',
-        # },
-    ) """
 else:
     print('get config from discovery url failed')
     exit()
 
+oauth2_scheme = OAuth2TokenBearer(tokenUrl=auth0_config["token_endpoint"])
+
+
+async def check_jwt(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="JWT credentials failed",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    payload = {}
+    try:
+        payload = jwt.decode(token, ks)
+        payload.validate()
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except ExpiredTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except DecodeError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="OAuth Authorization failure",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    return payload
+
+
+async def check_permissions(security_scopes: SecurityScopes, jwt_payload=Depends(check_jwt)):
+    jwt_permissions: list[str] = jwt_payload['permissions']
+    for scope in security_scopes.scopes:
+        if(not scope in jwt_permissions):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Missing permissions [" +
+                security_scopes.scope_str + "]",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+    return jwt_permissions
+
+""" async def get_permissions(request: Request, call_next):
+    print('check_permissions')
+    print(request)
+    return await call_next(request)
+
 
 @app.middleware("http")
 async def check_jwt(request: Request, call_next):
+    print('check_jwt')
     auth: str = request.headers.get('Authorization')
     token = None
     if auth:
@@ -56,15 +104,17 @@ async def check_jwt(request: Request, call_next):
         return Response('Unauthorized', 401)
 
     response = await call_next(request)
-    return response
+    return response """
 
 
 @app.get("/")
 def read_root():
-
+    # Not secured
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def read_item(item_id: int, q: Optional[str] = None, permissions=Security(check_permissions, scopes=['read:items'])):
+    # Secured
+    print('permissions ', permissions)
     return {"item_id": item_id, "q": q}
---
+++ oAuthTokenBearer.py
@@ -0,0 +1,35 @@
+from typing import Optional, Dict
+from fastapi import HTTPException, Request
+from starlette.status import HTTP_401_UNAUTHORIZED
+from fastapi.security import OAuth2
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.security.oauth2 import get_authorization_scheme_param
+
+
+class OAuth2TokenBearer(OAuth2):
+    def __init__(
+        self,
+        tokenUrl: str,
+        scheme_name: Optional[str] = None,
+        scopes: Optional[Dict[str, str]] = None,
+        auto_error: bool = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(
+            clientCredentials={"tokenUrl": tokenUrl, "scopes": scopes})
+        super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization: str = request.headers.get("Authorization")
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+        return param
